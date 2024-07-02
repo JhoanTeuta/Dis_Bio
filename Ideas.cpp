849188ac74d1d6e6f509d177767589d5c95371c6
JhoanTeuta <126626062+JhoanTeuta@users.noreply.github.com>
2024-07-02 13:48:08 -0500

    Update Ideas.cpp

--- Ideas.cpp
+++ Ideas.cpp
@@ -31,3 +31,20 @@ Un dispositivo de detección temprana de sepsis:
 Sensores de signos vitales: la temperatura corporal, la frecuencia cardíaca, la frecuencia respiratoria y la presión arterial.
 Sensores de bioquímicos: medir niveles de lactato, saturación de oxígeno (SpO2)
 Análisis de datos en tiempo real, Alertas y notificaciones, Historial de datos
+
+
+
+
+Calidad del Sueño, la actividad física durante el sueño, sino que también monitoriza el ambiente del sueño (temperatura, ruido, luminosidad)
+
+Calidad de aire (niveles de CO2, partículas, compuestos orgánicos volátiles)
+
+Alerta para Pacientes con Alzheimer, alertas de emergencia, recordatorios diarios personalizados (como comer, beber agua, realizar tareas) y enviar la ubicación del paciente a la nube para los cuidadores 
+
+Hidratación Podría usar tecnología de bioimpedancia.
+
+Análisis de Gases en el Aliento, detectar signos tempranos de enfermedades como la diabetes (a través de la acetona), problemas gastrointestinales, enfermedades respiratorias
+
+Monitoreo de la Postura
+
+Estrés, conductividad de la piel ...
