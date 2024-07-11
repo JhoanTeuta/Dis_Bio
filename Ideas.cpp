ae1b9747e4fb015106e076a27a91dd4ea6ee9751
JhoanTeuta <126626062+JhoanTeuta@users.noreply.github.com>
2024-07-10 21:09:54 -0500

    Update Ideas.cpp

--- Ideas.cpp
+++ Ideas.cpp
@@ -1,27 +1,11 @@
-Monitor de actividad física y sueño para rehabilitación
-Sistema de monitoreo de calidad del aire en hospitales
-Sensor de presión arterial conectado
-Sistema de alerta para caídas en pacientes mayores
-Monitor de hidratación en tiempo real
-Dispositivo portátil para análisis de gases en el aliento (por ejemplo, para la detección de enfermedades)
-Sistema de monitoreo y alerta para úlceras por presión
-Dispositivo de monitoreo de la postura para prevenir problemas de espalda
-Sensor de niveles de estrés basado en la conductividad de la piel
-
 Un dispositivo de detección temprana de sepsis:
 Sensores de signos vitales: la temperatura corporal, la frecuencia cardíaca, la frecuencia respiratoria y la presión arterial.
 Sensores de bioquímicos: medir niveles de lactato, saturación de oxígeno (SpO2)
 Análisis de datos en tiempo real, Alertas y notificaciones, Historial de datos
 
+Monitoreo calidad de aire (niveles de CO2, partículas, compuestos orgánicos volátiles)
 
-
-
-Calidad del Sueño, la actividad física durante el sueño, sino que también monitoriza el ambiente del sueño (temperatura, ruido, luminosidad)
-
-Calidad de aire (niveles de CO2, partículas, compuestos orgánicos volátiles)
-
-Alerta para Pacientes con Alzheimer, alertas de emergencia, recordatorios diarios personalizados (como comer, beber agua, realizar tareas) y enviar la ubicación del paciente a la nube para los cuidadores 
-
+Alerta para Pacientes con Alzheimer, alertas de emergencia, recordatorios diarios personalizados (como comer, beber agua, realizar tareas) y enviar la ubicación del paciente
 Hidratación Podría usar tecnología de bioimpedancia.
 
 Análisis de Gases en el Aliento, detectar signos tempranos de enfermedades como la diabetes (a través de la acetona), problemas gastrointestinales, enfermedades respiratorias
@@ -29,3 +13,5 @@ Análisis de Gases en el Aliento, detectar signos tempranos de enfermedades como
 Monitoreo de la Postura
 
 Estrés, conductividad de la piel ...
+
+Sistema de monitoreo y alerta para úlceras por presión
