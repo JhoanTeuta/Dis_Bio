06e89ba59286235067833d64dbebe24d3d745b1d
JhoanTeuta <126626062+JhoanTeuta@users.noreply.github.com>
2024-07-14 08:11:10 -0500

    Update Ideas.cpp

--- Ideas.cpp
+++ Ideas.cpp
@@ -5,7 +5,6 @@ Análisis de datos en tiempo real, Alertas y notificaciones, Historial de datos
 
 Monitoreo calidad de aire (niveles de CO2, partículas, compuestos orgánicos volátiles)
 
-Alerta para Pacientes con Alzheimer, alertas de emergencia, recordatorios diarios personalizados (como comer, beber agua, realizar tareas) y enviar la ubicación del paciente
 Hidratación Podría usar tecnología de bioimpedancia.
 
 Análisis de Gases en el Aliento, detectar signos tempranos de enfermedades como la diabetes (a través de la acetona), problemas gastrointestinales, enfermedades respiratorias
@@ -13,5 +12,3 @@ Análisis de Gases en el Aliento, detectar signos tempranos de enfermedades como
 Monitoreo de la Postura
 
 Estrés, conductividad de la piel ...
-
-Sistema de monitoreo y alerta para úlceras por presión
